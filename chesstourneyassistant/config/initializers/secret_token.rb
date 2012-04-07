c0faa75d297d5e96ac65f6d985fa6ae0c0896c03
Nicholas Parsons <nparsons_work@hotmail.com>
2012-04-07 09:31:15 -0400

    Inital Revision - Chess Tourney Assistant

---
+++ chesstourneyassistant/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chesstourneyassistant::Application.config.secret_token = 'f362eac3f51789bee62a2c2f380542e17930cd89bc552597dc9fa1289830bf3097686d35aa2223d4f08cc9d208c33b828068de46393130855bfc66a50efc5df1'
